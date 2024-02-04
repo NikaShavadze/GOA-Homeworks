740fbed87dab2d156e1809136eb9cc245ba913de
NikaShavadze <niikashavadze@gmail.com>
2024-02-04 20:21:23 +0400

    commit

---
+++ Day 1/info.py
@@ -0,0 +1,12 @@
+name = "Nika"
+surname = "Shavadze"
+age = 21
+height = 179.5
+weight = 75
+origin = "Batumi, Georgia"
+current_city = "Tbilisi"
+university = "Caucasus University"
+faculty = "computer science"
+hobby = "playing chess, playing basketball and boxing"
+
+print("Hello. My name is " + str(name) + " " + str(surname) + ". I am " + str(age) + " years old. I am from " + origin + ". Currently I live in " + current_city + ". I study " +  faculty + " at " + university + ". I have many hobbies, such as: " + hobby + ". I am " + str(height) + " cm tall. My weight is " + str(weight) + "kg. ")
