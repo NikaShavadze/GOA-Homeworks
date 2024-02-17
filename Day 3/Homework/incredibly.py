25371890a6cbf859df5e38f06c973d12cd85d935
NikaShavadze <niikashavadze@gmail.com>
2024-02-17 18:56:32 +0400

    commit

---
+++ Day 3/Homework/incredibly.py
@@ -0,0 +1,20 @@
+x = 1a
+y = 5c
+z = 10f
+age = 20s
+name = "nika
+surname = "shavadze
+fullname = name + surname-
+age2 = 20
+name2 = "nika"
+(print) hello world
+(print) good night
+print{age = 20}
+
+print(10s)
+print (age +)
+print(name2 -
+prin name)
+print(good morning)
+print(nanme 2)
+print(xzy
---
+++ Day 4/Homework/info.py
@@ -0,0 +1,6 @@
+name = "Nika"
+surname = "Shavadze"
+age = 21
+
+print("my name is " + name + ". My surname is " + surname + ". I am " + str(age) + 
+      " yeears old. In ten years I will be " + str(age + 10) + " years old.")
