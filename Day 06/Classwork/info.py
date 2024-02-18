c6f7864d945ec47ff9a1584c10abfea0d2980243
NikaShavadze <niikashavadze@gmail.com>
2024-02-18 18:44:37 +0400

    commit

---
+++ Day 06/Classwork/info.py
@@ -0,0 +1,6 @@
+name = input("Please enter your name: ")
+surname = input("Please enter your surname: ")
+age = input("Please enter your age: ")
+introduction = ("Hello. I am " + name + " " + surname + ". I am " + age + " years old")
+
+print(introduction)
---
+++ Day 06/Classwork/multiplication.py
@@ -0,0 +1,6 @@
+num1 = int(input("Please enter the 1st nubmer: "))
+num2 = int(input("Please enter the 2nd number: "))
+num3 = int(input("Please enter the 3rd number: "))
+result = (num1 * num2 * num3)
+
+print(result)
