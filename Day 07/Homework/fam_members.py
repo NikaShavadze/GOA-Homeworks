171ec8a09c0aac31a2dce59dd6a65cdd333584f6
NikaShavadze <niikashavadze@gmail.com>
2024-02-19 21:44:17 +0400

    commit

---
+++ Day 07/Homework/fam_members.py
@@ -0,0 +1,37 @@
+#Father
+father_name = "Roini"
+father_surname = "Shavadze"
+father_age = 47
+father_occupation = "lawyer"
+
+#Mother
+mother_name = "Maia"
+mother_surname = "Surnamidze"
+mother_age = 42
+mother_occupation = "housewife"
+
+#Sister
+sister_name = "Nini"
+sister_surname = "Shavadze"
+sister_age = 14
+sister_occupation = "student"
+
+print("My father is " + father_name, father_surname + ". He is " + str(father_age)
+      + " years old. He is a " + father_occupation + ". My mother is " + mother_name, 
+      mother_surname + "She is " + str(mother_age) + 
+      " years old. She was an English teacher, but now she is a " + mother_occupation +
+      ". My sister is " + sister_name, sister_surname + ". She is " + str(sister_age) +
+      " years old. She is a " + sister_occupation + ".")
+
+print(type(father_name))
+print(type(father_surname))
+print(type(father_age))
+print(type(father_occupation))
+print(type(mother_name))
+print(type(mother_surname))
+print(type(mother_age))
+print(type(mother_occupation))
+print(type(sister_name))
+print(type(sister_surname))
+print(type(sister_age))
+print(type(sister_occupation))
