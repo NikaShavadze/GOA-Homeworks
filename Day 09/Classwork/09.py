f3ed0a4abafc0ab05ef1caaa4b92a424fd5a84c8
NikaShavadze <niikashavadze@gmail.com>
2024-02-26 19:32:09 +0400

    commit

--- Day 09/Classwork/09.py
+++ Day 09/Classwork/09.py
@@ -1,17 +1,18 @@
-password = "12345"
-enter = input("Please enter a password")
-while enter != password:
-    enter = input("Please enter a password")
-print("true")
-
-
+# 1st way
 # password = "12345"
-# guess = input("Enter the password: ")
+# enter = input("Please enter a password")
+# while enter != password:
+#     enter = input("Please enter a password")
+# print("Access Granted!")
 
-# while True:
-#     if guess == password:
-#         print("Password correct.")
-#         break
-#     else:
-#          print("Incorrect password.")
-#          guess = input("Enter the password: ")
+# 2nd way
+password = "12345"
+guess = input("Enter the password: ")
+
+while True:
+    if guess == password:
+        print("Password correct.")
+        break
+    else:
+         print("Incorrect password.")
+         guess = input("Enter the password: ")
