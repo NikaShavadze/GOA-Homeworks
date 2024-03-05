873b8556c1ae116696618fadf6323cccb43db468
NikaShavadze <niikashavadze@gmail.com>
2024-03-05 19:25:27 +0400

    commit

--- Day 13/Homework/03.py
+++ Day 13/Homework/03.py
@@ -1,5 +1,5 @@
 """
-Write a program that asks the user to enter a password. 
+    Write a program that asks the user to enter a password. 
 If the password is "abc123", print "Access granted"; otherwise, print "Access denied".
 """
 
--- Day 13/Homework/05.py
+++ Day 13/Homework/05.py
@@ -1,5 +1,5 @@
 """
-Write a program that asks the user to enter a number between 1 and 5.
+    Write a program that asks the user to enter a number between 1 and 5.
 If the numberis less than 1 or greater than 5, print "Invalid input". 
 If the number is between 1 and 5, print "Valid input".
 """
