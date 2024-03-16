4476b68e83b55e85f235831633018fb60274d64e
NikaShavadze <niikashavadze@gmail.com>
2024-03-16 16:22:45 +0400

    commit

--- Day 17/Homework/07.py
+++ Day 17/Homework/07.py
@@ -7,6 +7,7 @@ returns the sum of all the numbers that are greater than 10.
 def sum_of_greater_than_10():
     my_list = input("Please enter numbers separated by space:\n")
     my_list = my_list.split()
+    sum = 0
     for i in my_list:
         if int(i) > 10:
             sum += int(i)
